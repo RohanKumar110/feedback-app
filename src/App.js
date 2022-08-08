b94b8769f627ddabbab14bde5e12508b8fa48731
rohankumar110 <rohanktalreja110@gmail.com>
2022-08-09 00:54:59 +0500

    add global state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
-      <div className="container">
-        <h1>My App</h1>
-      </div>
+      <div className="container">MyApp</div>
     </>
   );
 }
