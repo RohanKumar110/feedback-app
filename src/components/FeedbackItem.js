a8b3c86622f91d2e3c19ee1ad7a98c2f563d6301
rohankumar110 <rohanktalreja110@gmail.com>
2022-08-09 01:38:36 +0500

    add delete functionality in feedbackitem component

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+  const handleDeleteClick = (id) => {
+    handleDelete(id);
+  };
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDeleteClick(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text} </div>
     </Card>
   );
@@ -13,6 +21,7 @@ function FeedbackItem({ item }) {
 
 FeedbackItem.propTypes = {
   item: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
 
 export default FeedbackItem;
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
@@ -10,14 +10,21 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
 }
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.array,
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+  handleDelete: PropTypes.func.isRequired
 };
 
 export default FeedbackList;
