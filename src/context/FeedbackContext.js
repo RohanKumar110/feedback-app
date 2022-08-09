16356b238aa973f7645ab10addfffb5f24a28ebe
rohankumar110 <rohanktalreja110@gmail.com>
2022-08-09 22:16:50 +0500

    add update and delete functionality in json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -33,15 +33,26 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback]);
   };
 
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    });
+    const data = await response.json();
     const updatedFeedback = feedback.map((item) =>
-      item.id === id ? { ...item, ...updatedItem } : item
+      item.id === id ? { ...item, ...data } : item
     );
     setFeedback(updatedFeedback);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
       const newFeedback = feedback.filter((item) => item.id !== id);
       setFeedback(newFeedback);
     }
