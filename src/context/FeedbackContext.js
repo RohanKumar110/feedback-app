2336d6365c3ad885e9fdb49f7dd159cb82abae14
rohankumar110 <rohanktalreja110@gmail.com>
2022-08-09 22:11:22 +0500

    add create functionality in json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,8 +3,6 @@ import React, { createContext, useState, useEffect } from "react";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const BASE_URL = "http://localhost:5000";
-
   const [isLoading, setIsLoading] = useState(true);
   const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
@@ -18,14 +16,21 @@ export const FeedbackProvider = ({ children }) => {
   }, []);
 
   const fetchFeedback = async () => {
-    const response = await fetch(`${BASE_URL}/feedback?sort=id&_order=desc`);
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
   };
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = 4;
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
   };
 
   const updateFeedback = (id, updatedItem) => {
